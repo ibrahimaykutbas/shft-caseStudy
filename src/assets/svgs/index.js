cf5a871d0834ebfd6ea050cc899762c52bc27250
ibrahimaykutbas <ibrahimaykutbas@gmail.com>
2023-06-24 23:47:23 +0300

    Add new svg

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,7 +1,9 @@
 import menu from './menu.svg';
 import add from './add.svg';
 import back from "./back.svg";
+import share from "./share.svg";
 
 export const Menu = menu;
 export const Add = add;
 export const Back = back;
+export const Share = share;
